dd6a6b34bac4bd7e75e27247184ee58b86bfb6a0
alestor123 <alestoraldous@gmail.com>
2021-01-07 18:24:20 +0530

    added cli code

--- cli.js
+++ cli.js
@@ -1,1 +1,8 @@
 #!/usr/bin/env node
+
+var nv = require('./App');
+(async () => {
+var data = await nv();
+console.log(' \n Current Version : ' + data.current + '\n')
+console.log(' \n Latest Version : ' + data.latest + '\n')
+})();
